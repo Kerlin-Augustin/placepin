f45c78bc6e78ab0a88c02d12bc7ce2b035982910
Kerlin Augustin <kerlin.augustin1@gmail.com>
2025-10-02 18:41:58 -0400

    setting up database and backend pages

---
+++ backend/config/mongoDatabase.ts
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config()
+
+if (!process.env.MONGO_URI) {
+  throw new Error("MONGO_URI is not defined")
+}
+
+const Mongo_URI = process.env.MONGO_URI
+
+// Setup Mongoose
+
+export async function connectDB() {
+  try {
+    mongoose.set('strictQuery', false);
+    await mongoose.connect(Mongo_URI);
+    console.log("Connected to MongoDB via Mongoose");
+    console.log("Connected to database:", mongoose.connection.db?.databaseName);
+  } catch (err) {
+    console.error("MongoDB connection error:", err);
+    process.exit(1)
+  }
+}
---
+++ backend/server.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config()
+
+const app = express()
+const PORT = process.env.PORT || 3000
+
+// Middleware
+
+app.use(cors())
+app.use(express.json())
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
