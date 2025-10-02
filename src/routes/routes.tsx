a4a0da8c88486d95818695889845c4cbf59a62f1
Kerlin Augustin <kerlin.augustin1@gmail.com>
2025-10-02 16:46:50 -0400

    login route established

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,10 +1,12 @@
 import type { RouteObject } from "react-router-dom";
 import LandingPage from "../pages/home/LandingPage";
 import SignupPage from "../pages/auth/signup/SignupPage";
+import LoginPage from "../pages/auth/login/LoginPage";
 
 const routes: RouteObject[] = [
   { path: '/', element: <LandingPage /> },
   { path: '/signup', element: <SignupPage /> },
+  { path: '/login', element: <LoginPage /> },
 ]
 
 export default routes
