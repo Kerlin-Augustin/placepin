0475dd335bd90928bf005dc1166483478c457fd7
Kerlin Augustin <kerlin.augustin1@gmail.com>
2025-10-02 22:38:21 -0400

    created login page

--- src/pages/auth/login/LoginPage.tsx
+++ src/pages/auth/login/LoginPage.tsx
@@ -1,7 +1,80 @@
+import './loginPage.css'
+import axios from 'axios'
+import { useState } from 'react'
+import { NavLink, useNavigate } from 'react-router-dom';
+// import { useAuth } from '../../../context/AuthContext'
+
 const LoginPage = () => {
+
+  const navigate = useNavigate()
+  // const { login } = useAuth()
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [errorMessage, setErrorMessage] = useState('')
+
+  const handleLoginUser = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    const loginCredentials = {
+      email,
+      password
+    }
+
+    try {
+      const res = await axios.post(
+        '/api/login',
+        loginCredentials,
+      )
+
+      if (res.status === 200) {
+        // const { accessToken, email, username } = res.data
+        // login(email, accessToken, username)
+        navigate('/dashboard')
+      }
+    } catch (err: any) {
+      console.error(err)
+      setErrorMessage(err.response.data.message)
+    }
+  }
+
   return (
-    <div>
-      Login Page
+    <div className='entireContainer'>
+      <form onSubmit={handleLoginUser} className='login-heroForm'>
+        <h4>
+          Login
+        </h4>
+        <label
+          className='inputLabel'
+          htmlFor='email'
+        >
+          Email
+        </label>
+        <input
+          type="text"
+          className='inputFields'
+          onChange={(e) => setEmail(e.target.value.toLowerCase().trim())}
+          id='email'
+          required
+        />
+        <label
+          className='inputLabel'
+          htmlFor='password'
+        >
+          Password
+        </label>
+        <input
+          type="password"
+          className='inputFields'
+          onChange={(e) => setPassword(e.target.value)}
+          id='password'
+          required
+        />
+        <NavLink className='nav-link' to='/'><p className='forgotPassword'>Forgot Password?</p></NavLink>
+        <button>Submit</button>
+        <p className='errorMessage inputLabel'>{errorMessage}</p>
+        <p>Don't have an account? <NavLink to='/signup'>Sign up</NavLink></p>
+      </form>
     </div>
   )
 }
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -2,11 +2,13 @@ import type { RouteObject } from "react-router-dom";
 import LandingPage from "../pages/home/LandingPage";
 import SignupPage from "../pages/auth/signup/SignupPage";
 import LoginPage from "../pages/auth/login/LoginPage";
+import Dashboard from "../pages/dashboard/Dashboard";
 
 const routes: RouteObject[] = [
   { path: '/', element: <LandingPage /> },
   { path: '/signup', element: <SignupPage /> },
   { path: '/login', element: <LoginPage /> },
+  { path: '/dashboard', element: <Dashboard />}
 ]
 
 export default routes
