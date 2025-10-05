7bff5c1f2708aa3f80f7cbae2d9db9dfd15ec2c7
Kerlin Augustin <kerlin.augustin1@gmail.com>
2025-10-05 17:45:22 -0400

    convert to module on signup css

--- src/pages/auth/signup/SignupPage.tsx
+++ src/pages/auth/signup/SignupPage.tsx
@@ -1,4 +1,4 @@
-import './signupPage.css';
+import styles from './signupPage.module.css';
 import axios from 'axios';
 import { NavLink, useNavigate } from 'react-router-dom';
 import { useState } from 'react';
@@ -60,80 +60,80 @@ const SignupPage = () => {
   }
 
   return (
-    <div className='top-container'>
-      <section className='primarySection'>
-        <NavLink to='/' className='nav-link'>
-          <div className='signup-logo'>
+    <div className={styles.topContainer}>
+      <section className={styles.primarySection}>
+        <NavLink to='/' className={styles.navLink}>
+          <div className={styles.signupLogo}>
             PlacePin
           </div>
         </NavLink>
-        <h1 className='h1'>
+        <h1 className={styles.h1}>
           Elevating Homes, Simplifying Ownership.
         </h1>
         <ul>
-          <li className='li'>
-            <h4 className='h4'>Create living experiences people stay for</h4>
-            <p className='p'>Increase tenant satisfaction and retention with perks, services, and streamlined communication — all from one platform.</p>
+          <li className={styles.li}>
+            <h4 className={styles.h4}>Create living experiences people stay for</h4>
+            <p className={styles.p}>Increase tenant satisfaction and retention with perks, services, and streamlined communication — all from one platform.</p>
           </li>
 
-          <li className='li'>
-            <h4 className='h4'>Connect landlords and tenants through value</h4>
-            <p className='p'>From flexible plans to modern tools, PlacePin strengthens relationships and boosts the appeal of every unit.</p>
+          <li className={styles.li}>
+            <h4 className={styles.h4}>Connect landlords and tenants through value</h4>
+            <p className={styles.p}>From flexible plans to modern tools, PlacePin strengthens relationships and boosts the appeal of every unit.</p>
           </li>
 
-          <li className='li'>
-            <h4 className='h4'>Turn your property into a lifestyle upgrade</h4>
-            <p className='p'>Laundry, housekeeping, gym perks, and discounts — tenants get more than a place to stay, and landlords get happier residents.</p>
+          <li className={styles.li}>
+            <h4 className={styles.h4}>Turn your property into a lifestyle upgrade</h4>
+            <p className={styles.p}>Laundry, housekeeping, gym perks, and discounts — tenants get more than a place to stay, and landlords get happier residents.</p>
           </li>
 
-          <li className='li'>
-            <h4 className='h4'>Smarter housing starts with better tools</h4>
-            <p className='p'>Track rental income, enhance tenant experience, and manage perks without the complexity of traditional systems.</p>
+          <li className={styles.li}>
+            <h4 className={styles.h4}>Smarter housing starts with better tools</h4>
+            <p className={styles.p}>Track rental income, enhance tenant experience, and manage perks without the complexity of traditional systems.</p>
           </li>
         </ul>
 
       </section>
       <div>
-        <form onSubmit={handleCreateAccount} className='heroForm'>
+        <form onSubmit={handleCreateAccount} className={styles.heroForm}>
           <h4>
             Create your PlacePin account
           </h4>
           <label
-            className='inputLabel'
+            className={styles.inputLabel}
             htmlFor='name'
           >
             Full Name
           </label>
           <input
             type="text"
-            className='inputFields'
+            className={styles.inputFields}
             onChange={(e) => setUserName(e.target.value.toLowerCase())}
             id='name'
             placeholder='Dinah Johnson'
             required
           />
           <label
-            className='inputLabel'
+            className={styles.inputLabel}
             htmlFor='email'
           >
             Email
           </label>
           <input
             type="text"
-            className='inputFields'
+            className={styles.inputFields}
             onChange={(e) => setEmail(e.target.value.toLowerCase().trim())}
             id='email'
             placeholder="djohnson@gmail.com"
             required
           />
           <label
-            className='inputLabel'
+            className={styles.inputLabel}
             htmlFor='accountType'
           >
             Account Type
           </label>
           <select
-            className="inputFields"
+            className={styles.inputFields}
             onChange={(e) => setAccountType(e.target.value)}
             id="accountType"
             required
@@ -144,55 +144,55 @@ const SignupPage = () => {
           </select>
           {accountType === 'tenant' && <>
             <label
-              className='inputLabel'
+              className={styles.inputLabel}
               htmlFor='landlordPromo'
             >
               Landlord Promo Code
             </label>
             <input
               type="text"
-              className='inputFields'
+              className={styles.inputFields}
               onChange={(e) => setPromo(e.target.value.toLowerCase().trim())}
               id='landlordPromo'
               placeholder='1234-abc-5678 (Optional)'  />
           </>}
           <label
-            className='inputLabel'
+            className={styles.inputLabel}
             htmlFor='address'
           >
             Address
           </label>
           <input
             type="text"
-            className='inputFields'
+            className={styles.inputFields}
             onChange={(e) => setAddress(e.target.value.toLowerCase().trim())}
             id='address'
             placeholder='123 Main Street, Boston MA, 02136'
             required
           />
           <label
-            className='inputLabel'
+            className={styles.inputLabel}
             htmlFor='phoneNumber'
           >
             Phone Number
           </label>
           <input
             type="number"
-            className='inputFields'
+            className={styles.inputFields}
             onChange={(e) => setPhoneNumber(e.target.value.toLowerCase().trim())}
             id='phoneNumber'
             placeholder='617-555-5555'
             required
           />
           <label
-            className='inputLabel'
+            className={styles.inputLabel}
             htmlFor='password'
           >
             Password
           </label>
           <input
             type="password"
-            className='inputFields'
+            className={styles.inputFields}
             onChange={(e) => setPassword(e.target.value)}
             id='password'
             placeholder='********'
@@ -201,7 +201,7 @@ const SignupPage = () => {
           <br />
           <button>Submit</button>
           <p>Have an account? <NavLink to='/login'>Login</NavLink></p>
-          <p className='errorMessage inputLabel'>{errorMessage}</p>
+          <p className={`${styles.errorMessage} ${styles.inputLabel}`}>{errorMessage}</p>
         </form>
       </div>
     </div>
