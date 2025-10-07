b384a9d0a969e24680b81c8a7d4413cf1c8680db
Kerlin Augustin <kerlin.augustin1@gmail.com>
2025-10-07 03:15:42 -0400

    add settings icon to the header

--- src/components/headers/landlord/LandlordHeader.tsx
+++ src/components/headers/landlord/LandlordHeader.tsx
@@ -1,4 +1,5 @@
 import styles from './landlordHeader.module.css';
+import { Settings } from 'lucide-react';
 
 interface LandlordHeaderProps {
   username: string,
@@ -8,9 +9,14 @@ const LandlordHeader = ({ username }: LandlordHeaderProps) => {
 
   return (
     <div className={styles.landlordHeaderContainer}>
-      <h2>
+      <h2 className={styles.headerTitle}>
         Welcome, {username}
       </h2>
+      <Settings
+        size={30}
+        color={'black'}
+        className={styles.settingsIcon}
+      />
     </div>
   )
 }
