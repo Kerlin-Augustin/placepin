00ca609454ccbc98bda60cae4f30c1c5e93161e5
Kerlin Augustin <kerlin.augustin1@gmail.com>
2025-10-09 18:35:00 -0400

    fixed node not auto refreshing

--- backend/controllers/settings/landlordBasicInfoController.ts
+++ backend/controllers/settings/landlordBasicInfoController.ts
@@ -1,6 +1,8 @@
-import { type Request, type Response } from "express"
+import { type Request, type Response } from "express";
+import jwt from 'jsonwebtoken';
 
 export const landlordBasicInfoController = async (req: Request, res: Response) => {
   console.log(req.params.id)
+  console.log(jwt)
   res.status(200).json({ message: 'hello', test: 'test'})
 }
